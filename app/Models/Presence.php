822bca92c64834d44120a73e73597d7b567679bd
Jonathanwjy <jonathanwijaya062004@gmail.com>
2026-04-04 21:20:05 +0700

    presence model and relation

--- app/Models/Presence.php
+++ app/Models/Presence.php
@@ -8,9 +8,14 @@ class Presence extends Model
 {
     protected $fillable = [
         "employee_id",
-        "check_in",
-        "check_out",
         "date",
+        "check_in_time",
+        "check_out_time",
+        "status",
+        "clock_in_latitude",
+        "clock_in_longitude",
+        "clock_out_latitude",
+        "clock_out_longitude",
     ];
 
     public function employee()
